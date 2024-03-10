5703ff1b1d94ab22bc5726c5e51713d04f22db22
natalia zeballos <natalialeivaf@gmail.com>
2024-03-09 22:25:10 -0300

    modulo 5 hasta ejercicio 10

--- C5 Fundamentos Javascript/ejercicios/07.js
+++ C5 Fundamentos Javascript/ejercicios/07.js
@@ -2,7 +2,10 @@ function esTipoDato(valor) {
   // La función recibe un argumento "valor".
   // Retorna el tipo de dato de este valor.
   // Por ejemplo: "string", "number", "boolean", "object", etc.
-  // Tu código:
+  // Tu código:"
+  var tipo = typeof valor;
+  return tipo;
+
 }
 
 module.exports = esTipoDato;
--- C5 Fundamentos Javascript/ejercicios/10.js
+++ C5 Fundamentos Javascript/ejercicios/10.js
@@ -1,8 +1,7 @@
 function esFechaValida(fecha) {
-  // La función recibe un argumento "fecha".
-  // Comprueba si este corresponde a una fecha válida.
-  // Si es así, retorna true, sino retorna false.
-  // Tu código:
+  return fecha instanceof Date && !isNaN(fecha.getTime());
 }
+
+
 
 module.exports = esFechaValida;
