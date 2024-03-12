64de379d9de74704d3c5945c516b213d8095f0e9
natalia zeballos <natalialeivaf@gmail.com>
2024-03-11 22:52:20 -0300

    ejercicio del 18 a 35

--- C5 Fundamentos Javascript/ejercicios/11.js
+++ C5 Fundamentos Javascript/ejercicios/11.js
@@ -5,9 +5,15 @@ function esMayorDeEdad(fechaNacimiento) {
   // Devuelve true si lo es, caso contrario, retorna false.
   // PISTA: Investiga el método getFullYear() de la clase Date para obtener el año actual.
   // Tu código:
+
+  //aca declaramos la fecha actual
   var fechaActual= new Date();
   var Año= fechaActual.getFullYear(); //2024
+
+  //aca declaramos la fecha de nacimiento
   var añoNacimiento= fechaNacimiento.getFullYear();//año que ingresa el usuario
+
+  //aca comparamos la fechas y retornamos el valor
   if(Año-añoNacimiento >=18){
     return true;
   }
--- C5 Fundamentos Javascript/ejercicios/20.js
+++ C5 Fundamentos Javascript/ejercicios/20.js
@@ -3,6 +3,24 @@ function esVocal(letra) {
   // Si el usuario ingresa un string de más de un caracter, debes retornar el mensaje: "Dato incorrecto".
   // Si no es vocal, tambien debe retornar "Dato incorrecto".
   // Tu código:
-}
+  if(letra==="a" && letra.length ===1){
+    return "Es vocal";
+  }
+  else if(letra==="e" && letra.length ===1){
+    return "Es vocal";
+  }
+  else if(letra==="i" && letra.length ===1){
+    return "Es vocal";
+  }
+  else if(letra==="o" && letra.length ===1){
+    return "Es vocal";
+  }
+  else if(letra==="u" && letra.length ===1){
+    return "Es vocal";
+  }
+  else {
+    return "Dato incorrecto";
+  }
 
+}
 module.exports = esVocal;
